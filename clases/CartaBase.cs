424b5382199d10c5d234cf4414971376b1c0f309
zA1ejandro <alexmocu241106@gmail.com>
2025-11-03 10:57:10 -0600

    Creacion de la Carta Base

---
+++ clases/CartaBase.cs
@@ -0,0 +1,26 @@
+
+namespace BlackJack_Uno.clases;
+
+abstract class CartaBase
+{
+    protected int _valor;
+    public abstract int Valor { get; set; }
+
+    public enum Colores
+    {
+        Azul, Amarillo, Rojo, Verde, Negro
+    }
+
+    protected Colores _color;
+    public abstract Colores Color { get; set; }
+
+    protected string _figura = "";
+    public string Figura { get; set; }
+
+    protected CartaBase(int valorCarta, Colores colorCarta, string figuraCarta)
+    {
+        Valor = valorCarta;
+        Color = colorCarta;
+        Figura = figuraCarta;
+    }
+}
