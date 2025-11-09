12eb2bb72c3a73cfeaed8ff6334a8cc54cc461b2
MRef02 <marifercan73@gmail.com>
2025-11-08 18:31:22 -0600

    clase Abstracta de el jugador
    se creo la clase base de del jugador

---
+++ clases/BaseJugador.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using BlackJack_Uno.Interfaces;
+
+namespace BlackJack_Uno.clases
+{
+    public abstract class BaseJugador
+    {
+        public string TipoJugador { get; private set; }
+         public Mano Mano { get;  set; }
+
+        protected BaseJugador(string tipoJugador)
+        {
+            TipoJugador = tipoJugador;
+            Mano = new Mano();
+}
